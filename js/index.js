86fc077d7c11290a857b4f0302d1387e0c1be1b2
Ricardo Mendieta <mendietaricardo9@gmail.com>
2020-07-15 06:28:06 -0500

    feat: removed jquery and performance optmizations

--- js/index.js
+++ js/index.js
@@ -1,52 +1,38 @@
-$(function () {
-  var $cursor = $(".cursor"),
-    $overlay = $(".overlay");
+window.addEventListener("DOMContentLoaded", init);
 
-  function moveCircle(e) {
-    TweenLite.to($cursor, 0.5, {
-      css: {
-        left: e.pageX,
-        top: e.pageY,
-      },
-      delay: 0.03,
+function initCursor() {
+  var cursor = document.querySelector(".cursor");
+  var overlay = document.querySelector(".experiences");
+
+  function onMouseEnter() {
+    gsap.to(cursor, {scale: 1, autoAlpha: 1, duration: 0.3, ease: "back.out"});
+  }
+
+  function onMouseMove(e) {
+    gsap.to(cursor, {
+      duration: 0.5,
+      x: e.clientX,
+      y: e.clientY,
+      ease: "power2.easeOut"
     });
   }
 
-  $(".e-1").hover(function () {
-    $(".cursor").css({
-      "background-image":
-        "url(https://i.pinimg.com/564x/85/24/d7/8524d785a8427617d475bf02d51710fc.jpg)",
-    });
-  });
+  function onMouseLeave() {
+    gsap.to(cursor, {scale: 0.1, autoAlpha: 0, duration: 0.3, ease: "back.in"});
+  }
 
-  $(".e-2").hover(function () {
-    $(".cursor").css({
-      "background-image":
-        "url(https://i.pinimg.com/564x/97/59/85/9759859a26a8f8195d1c4dd92f00cb73.jpg)",
+  var elements = document.querySelectorAll("[data-cursor]");
+  elements.forEach(function (element) {
+    element.addEventListener("mouseenter", function () {
+      cursor.style.backgroundImage = "url(" + element.dataset.cursor + ")";
     });
-  });
-  $(".e-3").hover(function () {
-    $(".cursor").css({
-      "background-image":
-        "url(https://i.pinimg.com/564x/9c/52/81/9c528158c74da06541565671cfc2644b.jpg)",
-    });
-  });
-  $(".e-4").hover(function () {
-    $(".cursor").css({
-      "background-image":
-        "url(https://i.pinimg.com/564x/38/18/c3/3818c31969226e29a9dabd5e3cd0802a.jpg)",
-    });
-  });
+  })
 
-  var flag = false;
-  $($overlay).mousemove(function () {
-    flag = true;
-    TweenLite.to($cursor, 0.3, { scale: 1, autoAlpha: 1 });
-    $($overlay).on("mousemove", moveCircle);
-  });
+  overlay.addEventListener("mouseenter", onMouseEnter);
+  overlay.addEventListener("mousemove", onMouseMove);
+  overlay.addEventListener("mouseleave", onMouseLeave);
+}
 
-  $($overlay).mouseout(function () {
-    flag = false;
-    TweenLite.to($cursor, 0.3, { scale: 0.1, autoAlpha: 0 });
-  });
-});
+function init() {
+  initCursor();
+}
